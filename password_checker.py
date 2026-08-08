53f797c0f95d53ce262f85b5b841534871c11ac2
joanshetila <joanshetila@gmail.com>
2026-08-08 21:55:45 +0530

    Add password strength evaluation logic
    Implement password strength checker based on criteria.

---
+++ password_checker.py
@@ -0,0 +1,18 @@
+password = input("Enter a password")
+score = 0
+if len(password)>=8:
+  score+=1
+if any(char.isupper()) for char in password):
+  score+=1
+if any(char.islower() for char in password):
+  score+=1
+if any(char.isdigit() for char in password):
+  score+=2
+if any(not char.isalnum() for char in password):
+  score+=1
+if score<=2:
+  print("Password strength:Weak")
+elif scor<=4:
+  print("Password strength:Moderate")
+else:
+  print("Password strength:Strong")
