0f164cc00cdd42004ec8120be0384ad0594c6ad1
joanshetila <joanshetila@gmail.com>
2026-08-11 22:32:02 +0530

    Add files via upload

--- password_checker.py
+++ password_checker.py
@@ -2,7 +2,7 @@ password = input("Enter a password")
 score = 0
 if len(password)>=8:
   score+=1
-if any(char.isupper()) for char in password):
+if any(char.isupper() for char in password):
   score+=1
 if any(char.islower() for char in password):
   score+=1
@@ -12,7 +12,7 @@ if any(not char.isalnum() for char in password):
   score+=1
 if score<=2:
   print("Password strength:Weak")
-elif scor<=4:
+elif score<=4:
   print("Password strength:Moderate")
 else:
   print("Password strength:Strong")
